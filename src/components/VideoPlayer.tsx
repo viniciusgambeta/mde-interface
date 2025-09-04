7bf53ee0f88cb9bb1422dc528f3a30b80ef8bd70
viniciusgambeta <152810391+viniciusgambeta@users.noreply.github.com>
2025-09-04 13:23:41 -0300

    Fix instructor name spacing

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -363,7 +363,7 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ video, onBack, onVideoSelect 
               />
               <div>
                 <div className="flex items-center space-x-1">
-                  <span className="text-white font-semibold text-xl pl-2" style={{ marginBottom: '-3px' }}>{currentVideo.instructor?.name || 'Instrutor'}</span>
+                  <span className="text-white font-semibold text-xl" style={{ marginBottom: '0px', marginLeft: '0px', paddingLeft: '0px' }}>{currentVideo.instructor?.name || 'Instrutor'}</span>
                 </div>
                 <div className="flex items-center mt-0 pl-[3px]">
                   {currentVideo.instructor?.social_instagram && (
