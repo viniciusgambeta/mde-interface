d559db4bf7e03148028d615afc5d287a33f4adfd
viniciusgambeta <152810391+viniciusgambeta@users.noreply.github.com>
2025-10-01 20:11:26 -0300

    Reduce spacing between description and grid in Poderzinhos page

--- src/components/PoderzinhosPage.tsx
+++ src/components/PoderzinhosPage.tsx
@@ -165,7 +165,7 @@ const PoderzinhosPage: React.FC = () => {
       </div>
 
       {/* Ferramentas Grid */}
-      <div className="mt-16">
+      <div className="mt-8">
         {ferramentas.length > 0 ? (
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-32">
             {ferramentas.map((ferramenta, index) => (
