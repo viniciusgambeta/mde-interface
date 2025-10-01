eb1ac24ed5c4252fdc6935f0162f3235bac444aa
viniciusgambeta <152810391+viniciusgambeta@users.noreply.github.com>
2025-10-01 20:13:28 -0300

    Add moderate spacing between description and grid in Poderzinhos

--- src/components/PoderzinhosPage.tsx
+++ src/components/PoderzinhosPage.tsx
@@ -165,7 +165,7 @@ const PoderzinhosPage: React.FC = () => {
       </div>
 
       {/* Ferramentas Grid */}
-      <div className="mt-4">
+      <div className="mt-8">
         {ferramentas.length > 0 ? (
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
             {ferramentas.map((ferramenta, index) => (
